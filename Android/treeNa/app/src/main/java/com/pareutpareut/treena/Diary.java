5d7d4582f0ebc14e2605b84c8da10737b8253682
asong57 <1998thddk@gmail.com>
2021-06-26 23:52:10 +0900

    diary 데이터베이스 연결

--- Android/treeNa/app/src/main/java/com/pareutpareut/treena/Diary.java
+++ Android/treeNa/app/src/main/java/com/pareutpareut/treena/Diary.java
@@ -3,6 +3,17 @@ package com.pareutpareut.treena;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Diary extends AppCompatActivity {
 
@@ -10,5 +21,33 @@ public class Diary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        Date currentTime = new Date(System.currentTimeMillis());
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd", Locale.getDefault());
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM", Locale.getDefault());
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.getDefault());
+
+        String year = yearFormat.format(currentTime);
+        String month = monthFormat.format(currentTime);
+        String day = dayFormat.format(currentTime);
+
+        String today = year + "년 " + month + "월 " + day + "일 ";
+        String date = year + month + day;
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(today);
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+        EditText editText = findViewById(R.id.editText);
+        Button button_save = findViewById(R.id.button_save);
+        button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("diary").child(date).setValue(editText.getText().toString());
+            }
+        });
+
     }
 }

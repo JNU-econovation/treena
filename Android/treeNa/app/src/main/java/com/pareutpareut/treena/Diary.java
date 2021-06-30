e0be5532e0116ac7aed3a1e826d7a8c175cc8bce
asong57 <1998thddk@gmail.com>
2021-06-30 15:38:50 +0900

    데이터베이스에서 일기 읽어오기

--- Android/treeNa/app/src/main/java/com/pareutpareut/treena/Diary.java
+++ Android/treeNa/app/src/main/java/com/pareutpareut/treena/Diary.java
@@ -1,18 +1,27 @@
 package com.pareutpareut.treena;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class Diary extends AppCompatActivity {
@@ -42,10 +51,27 @@ public class Diary extends AppCompatActivity {
 
         EditText editText = findViewById(R.id.editText);
         Button button_save = findViewById(R.id.button_save);
+
+        //데이터베이스에 일기 저장
         button_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 databaseReference.child("diary").child(date).setValue(editText.getText().toString());
+                Toast.makeText(getApplicationContext(), "일기가 저장되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //데이터베이스에서 일기 읽어오
+        databaseReference.child("diary").child(date).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                } else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    String diary = String.valueOf(task.getResult().getValue());
+                    editText.setText(diary);
+                }
             }
         });
 
